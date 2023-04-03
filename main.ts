172165c7c2dd45f2d2a9a2cdb37b8eb3bc040d41
DavidUdic-P1A <115144133+DavidUdic-P1A@users.noreply.github.com>
2023-04-03 23:05:25 +0200

    Aktualizovat main.ts

--- main.ts
+++ main.ts
@@ -1,30 +1,30 @@
-let scoreA = 0 // skóre hráče A
-let scoreB = 0 // skóre hráče B
+let scoreA = 0 
+let scoreB = 0 
 let gameStarted = false
 
 
 function showIcon() {
-    let icon = Math.randomRange(0, 1) 
-    gameStarted = true 
-    basic.clearScreen() 
-    basic.showIcon(icon) 
-    basic.pause(2000) 
-    basic.clearScreen() 
+    let icon = Math.randomRange(0, 1)
+    gameStarted = true
+    basic.clearScreen()
+    basic.showIcon(icon)
+    basic.pause(2000)
+    basic.clearScreen()
 }
 
 
 function checkButton() {
-    let buttonPressed = false 
-    let buttonA = input.buttonIsPressed(Button.A) 
-    let buttonB = input.buttonIsPressed(Button.B) 
-    if (buttonA && !buttonB) { 
+    let buttonPressed = false
+    let buttonA = input.buttonIsPressed(Button.A)
+    let buttonB = input.buttonIsPressed(Button.B)
+    if (buttonA && !buttonB) {
         buttonPressed = true
-        if (gameStarted) { 
+        if (gameStarted) {
             scoreA += 1
         }
     } else if (buttonB && !buttonA) {
         buttonPressed = true
-        if (gameStarted) { 
+        if (gameStarted) {
             scoreB += 1
         }
     }
@@ -33,13 +33,32 @@ function checkButton() {
 
 
 function showWinner() {
-    gameStarted = false 
+    gameStarted = false
     if (scoreA > scoreB) {
         basic.showString("A WIN!")
-    } else if (scoreB > scoreA) { 
+    } else if (scoreB > scoreA) {
         basic.showString("B WIN!")
-    } else { 
+    } else {
         basic.showString("TIE!")
     }
-    basic.pause(3000) 
+    basic.pause(3000)
+
 }
+
+basic.forever(function () {
+    if (!gameStarted) { 
+        basic.showString("PRESS A OR B")
+        while (!checkButton()) {
+            
+        }
+        showIcon()
+    } else { 
+        if (input.buttonIsPressed(Button.AB)) { 
+            showWinner() 
+            scoreA = 0 
+            scoreB = 0
+        } else { 
+            checkButton()
+        }
+    }
+})
